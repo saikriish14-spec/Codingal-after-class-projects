e725076a380db77dd008cd27ab136b3bda28db6d
saikriish14-spec <saikriish14@gmail.com>
2026-02-23 20:53:12 +0000

    ok

---
+++ project1.py
@@ -0,0 +1,15 @@
+# Ask the user for the temperature
+
+# use conditional statements to decide which clothes to wear 
+
+temperature = float(input("Enter the temperature  : "))
+
+if temperature >= 25:
+    print("It's warm You can wear light clothes.")
+elif 15 <= temperature < 25:
+    print("It's mild wear a light jacket.")
+else:
+    print("It's cold  wear warm clothes")
+
+
+
