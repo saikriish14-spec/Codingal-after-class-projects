af03f42de6d9d421f27a0009e9afcfca574e74bf
saikriish14-spec <saikriish14@gmail.com>
2026-02-24 21:14:35 +0000

    ok

--- project2.py
+++ project2.py
@@ -1,1 +1,16 @@
-# Raj's frien's birthday dates
+# Raj's friend's birthday dates
+
+Jake = "january 25th "
+jack = "april 2nd"
+bob = "June 1"
+nicolls = "1st of may"
+mark = "1st of January"
+
+
+print(f"Jake's birthday is {Jake}")
+print(f"Jack's birthday is {jack}")
+print(f"bob's birthday is {bob}")
+print(f"nicolls birthday is {nicolls}")
+print(f"mark's birthday is {mark}")
+
+
