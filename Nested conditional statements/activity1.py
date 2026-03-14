ab73a1ed8ecd8d0bf87b38035aa3e8bf950418df
saikriish14-spec <saikriish14@gmail.com>
2026-03-14 15:25:08 +0000

    ok

---
+++ Nested conditional statements/activity1.py
@@ -0,0 +1,12 @@
+age = int(input("Please enter your age: "))
+if age >= 18:
+    if age >= 21:
+        print("You are an adult over 21 years old")
+    else:
+        print("You are an adult but under 21 years old")
+else:
+    print("You are a minor")
+    
+        
+          
+            
