2b71e04627dde5e9cf736cd8368a36d3c6758fd4
saikriish14-spec <saikriish14@gmail.com>
2026-05-18 21:20:29 +0100

    ok

---
+++ Square/import turtle.py
@@ -0,0 +1,20 @@
+import turtle 
+COD= turtle.Turtle() 
+COD.color("cyan")
+COD.speed(0) 
+
+COD.left(90)
+COD.forward(100)    
+COD.right(90)
+COD.forward(100)    
+COD.right(90)   
+COD.forward(100)    
+COD.right(90)   
+COD.forward(100)    
+
+
+
+
+
+
+turtle.done()   
