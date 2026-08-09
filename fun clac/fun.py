98e76a6be62208213081a0cab04aa163293a1d12
saikriish14-spec <saikriish14@gmail.com>
2026-08-09 18:26:03 +0100

    ok

---
+++ fun clac/fun.py
@@ -0,0 +1,23 @@
+print("Simple Calculator")
+
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+if operator == "+":
+    print("Answer:", num1 + num2)
+
+elif operator == "-":
+    print("Answer:", num1 - num2)
+
+elif operator == "*":
+    print("Answer:", num1 * num2)
+
+elif operator == "/":
+    if num2 != 0:
+        print("Answer:", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero.")
+
+else:
+    print("Invalid operator.")
